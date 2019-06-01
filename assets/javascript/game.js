a40cb914354dbfa27f1e6ba3b45603fdeaea54ed
developrfw <developrfw@gmail.com>
2019-06-01 11:28:23 -0600

    ...

---
+++ assets/javascript/game.js
@@ -0,0 +1,94 @@
+$(document).ready(function () {
+
+    var possibleWords = ["Neo", "Trinity", "Morpheus", "Cypher", "Tank", "Machine", "Agent", "Trainman", "Keymaker", "Matrix", "Oracle"]
+
+    const maxGuess = 10
+    var pauseGame = false
+
+    var guessedLetters = []
+    var guessingWord = []
+    var wordToMatch
+    var numGuess
+    var wins = 0
+
+    resetGame()
+
+
+    document.onkeypress = function (event) {
+        if (isAlpha(event.key) && !pauseGame) {
+            checkForLetter(event.key.toUpperCase())
+        }
+    }
+
+    function checkForLetter(letter) {
+        var foundLetter = false
+
+
+        for (var i = 0, j = wordToMatch.length; i < j; i++) {
+         if (letter === wordToMatch[i]) {
+                guessingWord[i] = letter
+                foundLetter = true
+                
+
+            if (guessingWord.join("") === wordToMatch) {
+                 wins++
+                pauseGame = true
+                  updateDisplay()
+                setTimeout(resetGame, 5000)
+                }
+            }
+        }
+
+        if (!foundLetter) {
+            
+
+            if (!guessedLetters.includes(letter)) {
+                guessedLetters.push(letter)
+                numGuess--
+            }
+            if (numGuess === 0) {
+
+            guessingWord = wordToMatch.split()
+             pauseGame = true
+            setTimeout(resetGame, 5000)
+            }
+        }
+
+        updateDisplay()
+
+    }
+
+    function isAlpha(ch) {
+        return /^[A-Z]$/i.test(ch);
+    }
+
+    function resetGame() {
+        numGuess = maxGuess
+        pauseGame = false
+
+
+        wordToMatch = possibleWords[Math.floor(Math.random() * possibleWords.length)].toUpperCase()
+        console.log(wordToMatch)
+
+
+        guessedLetters = []
+        guessingWord = []
+
+        for (var i = 0, j = wordToMatch.length; i < j; i++) {
+            if (wordToMatch[i] === " ") {
+                guessingWord.push(" ")
+            } else {
+                guessingWord.push("_")
+            }
+        }
+
+        updateDisplay()
+    }
+
+    function updateDisplay() {
+        document.getElementById("totalWins").innerText = wins
+        document.getElementById("currentWord").innerText = guessingWord.join("")
+        document.getElementById("remainingGuesses").innerText = numGuess
+        document.getElementById("guessedLetters").innerText = guessedLetters.join(" ")
+    }
+})
